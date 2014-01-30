4c7c459ee005271e5187c53cc82448729ea76aed
Xkeeper <xkeeper@gmail.com>
2014-01-29 16:57:50 -0800

    Changing window size to 640x480 for now.
    Some debug output was also removed.

--- conf.lua
+++ conf.lua
@@ -2,7 +2,10 @@
 --- Callback function for configuring LOVE.
 function love.conf(t)
 	t.author		= "Xkeeper"
-	love_version	= "0.9.0"
+	t.version		= "0.9.0"
 	t.title			= "Columns Clone"
 	t.identity		= "columns"
+
+	t.window.width	= 640
+	t.window.height	= 480
 end
